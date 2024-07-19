771429aa309ea04f755e330a858c59a67e151259
devusmonjon <usmonjonhasanov777@gmail.com>
2024-07-19 16:27:35 +0500

    fark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <body className={montserrat.className}>
         <Navbar />
         {children}
